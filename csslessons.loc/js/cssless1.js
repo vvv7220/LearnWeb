aa375ba7108e0173c2ce255096f2d7da207c7f71
vvv7220 <vvv7220@gmail.com>
2018-11-21 20:49:39 +0800

    21.11.2018
    20:50 Болит голова

---
+++ csslessons.loc/js/cssless1.js
@@ -0,0 +1,6 @@
+let dozer = document.getElementById('dozer');
+let code = document.getElementById('code');
+window.onclick = function () {
+dozer.style.left = 1400 + 'px';
+code.style.top = 130 +'px';
+}
