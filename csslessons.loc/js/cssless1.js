e07559a00e6aec5619540b744cf59e04dab4f1ab
vvv7220 <vvv7220@gmail.com>
2018-11-24 11:09:52 +0800

    24112018 11:10 am
    Создал файл csslesson2 для отработки display:flex

--- csslessons.loc/js/cssless1.js
+++ csslessons.loc/js/cssless1.js
@@ -1,6 +1,7 @@
 let dozer = document.getElementById('dozer');
 let code = document.getElementById('code');
+let h = document.getElementsByTagName('h1')[0];
 window.onclick = function () {
 dozer.style.left = 1400 + 'px';
-code.style.top = 130 +'px';
+h.style.opacity = 0;
 }
