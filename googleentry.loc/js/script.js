eca3325eec94ad5f03f5084fc71efbab3d562330
vvv7220 <vvv7220@gmail.com>
2018-12-22 17:40:47 +0800

    22122018

--- googleentry.loc/js/script.js
+++ googleentry.loc/js/script.js
@@ -2,3 +2,9 @@
 // crt.onmouseover = function () {
 //     crt.style.opacity = "0.2";
 // }
+let polosa = document.getElementById('polosa');
+let next = document.querySelector('.nxt');
+next.onclick = function(){
+polosa.style.left = -452 + 'px';
+}
+
